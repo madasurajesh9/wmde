fb7e6ac0aad8f8da0bcbdff15123c7631896a98a
madasurajesh9 <madasu.rajesh@japsinfotech.com>
2014-04-17 17:35:05 +0530

    changed
    changed something

---
+++ ScrollViewListener.java
@@ -0,0 +1,9 @@
+package com.japsinfotech.f4skools;
+
+public interface ScrollViewListener {
+
+	void onScrollChanged(ObservableScrollView scrollView, int x, int y,
+			int oldx, int oldy);
+
+			done changes
+}
